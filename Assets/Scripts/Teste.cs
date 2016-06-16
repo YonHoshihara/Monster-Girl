2cb79436861b3149ecb6f7ba4017a13242075d29
YonHoshihara <hoshiharyon@gmail.com>
2016-06-15 21:07:06 -0700

    Monster girl
    Animation

---
+++ Assets/Scripts/Teste.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teste : MonoBehaviour {
+    public float[] MusicTeste;
+    private int cont=0;
+	// Use this for initialization
+	void Start () {
+        MusicTeste = GetComponent<AudioAnalisys>().MusicAnaliseVector();
+        // InvokeRepeating("ContadorSegundo",0,1);
+        for (int i=0;i<MusicTeste.Length;i++)
+        {
+           Debug.Log( MusicTeste[i]);
+
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+   
+
+
+}
