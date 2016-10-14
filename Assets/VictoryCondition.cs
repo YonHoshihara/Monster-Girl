0af09381f05917522459192c52ac38f96fdac0b8
Amaterisa <jucimarmaiasilvajr@Mac-mini-de-Ludus-3.local>
2016-10-14 13:56:02 -0400

    tela de vitoria, etc
    vitoria

---
+++ Assets/VictoryCondition.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class VictoryCondition : MonoBehaviour {
+	private AudioSource audio;
+	public string scene;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!audio.isPlaying){
+
+			SceneManager.LoadScene (scene);
+		}
+	}
+}
